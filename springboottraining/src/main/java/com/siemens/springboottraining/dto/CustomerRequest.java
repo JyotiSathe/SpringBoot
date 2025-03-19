6e8937cf392c8591c0678e923d37a23560ed1e38
JyotiSathe <jyotisathe1138@gmail.com>
2025-03-19 11:17:04 +0000

    swagger configuration

--- springboottraining/src/main/java/com/siemens/springboottraining/dto/CustomerRequest.java
+++ springboottraining/src/main/java/com/siemens/springboottraining/dto/CustomerRequest.java
@@ -1,5 +1,6 @@
 package com.siemens.springboottraining.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -11,6 +12,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerRequest {
+
+    @Schema(hidden = true)
     protected long customerId;
 
     protected FullNameRequest fullName;
@@ -23,5 +26,6 @@ public class CustomerRequest {
 
     @NotNull(message = "password cannot be null")
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$", message = "Password format not matching")
+    @Schema(example = "Test@123", defaultValue = "")
     protected String password;
 }
--- springboottraining/src/main/java/com/siemens/springboottraining/entity/Corporate.java
+++ springboottraining/src/main/java/com/siemens/springboottraining/entity/Corporate.java
@@ -16,7 +16,6 @@ import lombok.experimental.SuperBuilder;
 @Entity
 public class Corporate extends Customer {
 
-
     @Enumerated(EnumType.STRING)
     @Column(name = "CompanyType")
     private CompanyType companyType;
