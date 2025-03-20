8f5159c7b1cc0dc34e5c0d4b512fce02c1182e5d
JyotiSathe <35442393+JyotiSathe@users.noreply.github.com>
2025-03-20 16:26:28 +0530

    eureka server demo

---
+++ eurekaserverdemo/src/main/java/com/siemens/eurekaserverdemo/EurekaserverdemoApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.eurekaserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaserverdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaserverdemoApplication.class, args);
+    }
+
+}
