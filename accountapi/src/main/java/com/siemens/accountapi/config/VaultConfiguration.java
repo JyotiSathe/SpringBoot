acbe120235dcbcb29d477ffc343bad06fb09c159
Jyoti <jyotisathe1138@gmail.com>
2025-03-20 12:53:08 +0530

    vault configuration

---
+++ accountapi/src/main/java/com/siemens/accountapi/config/VaultConfiguration.java
@@ -0,0 +1,16 @@
+package com.siemens.accountapi.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ConfigurationProperties
+public class VaultConfiguration {
+
+    private String username;
+    private String password;
+}
