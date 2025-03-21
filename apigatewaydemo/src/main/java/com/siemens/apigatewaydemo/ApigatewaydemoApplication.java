60652163b41241fa7e11b5a7a732cb1cad9a386e
Jyoti <jyotisathe1138@gmail.com>
2025-03-21 10:24:05 +0530

    initial setup for api gateway

--- apigatewaydemo/src/main/java/com/siemens/apigatewaydemo/ApigatewaydemoApplication.java
+++ apigatewaydemo/src/main/java/com/siemens/apigatewaydemo/ApigatewaydemoApplication.java
@@ -2,8 +2,10 @@ package com.siemens.apigatewaydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApigatewaydemoApplication {
 
     public static void main(String[] args) {
